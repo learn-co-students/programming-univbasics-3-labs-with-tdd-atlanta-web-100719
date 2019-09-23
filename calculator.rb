a67e5f842c9213072e3951723ac0e8c38afd6de0
Kuran V Kumar <kurankumar@gmail.com>
2019-09-23 21:36:47 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,21 @@
 # Add your variables here
+
+first_number = 5
+second_number = 10.4
+
+sum = first_number + second_number
+
+puts sum
+
+difference = first_number - second_number
+
+puts difference
+
+product = first_number * second_number
+
+puts product
+
+quotient = first_number / second_number
+
+puts quotient
+
